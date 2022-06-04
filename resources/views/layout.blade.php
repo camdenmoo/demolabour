30bbaf544546e2625928f8388b11bd4aaa0c5755
frank jones <frankjones.web@gmail.com>
2022-06-04 11:10:55 +0100

    Converted alpine.js to be included in app.js instead of using the CDN.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo Site for the Labour Party</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+
+    
+</body>
+</html>
