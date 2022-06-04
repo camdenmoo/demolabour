057fcf513bd7d153471d69fec4b7135a7b65b407
frank jones <frankjones.web@gmail.com>
2022-06-04 17:16:02 +0100

    HomeController added and route methods moved over

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,11 +1,15 @@
-<div class="bg-dark text-secondary px-4 py-5 text-center">
+<div class="bg-dark text-secondary px-4 py-5 text-center labour-hero">
     <div class="py-5">
         <h1 class="display-5 fw-bold text-white">Labour Party Events</h1>
         <div class="col-lg-6 mx-auto">
             <p class="fs-5 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure.</p>
             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Custom button</button>
-                <button type="button" class="btn btn-outline-light btn-lg px-4">Secondary</button>
+                <a href="/events">
+                    <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">View events</button>
+                </a>
+                <a href="/">
+                    <button type="button" class="btn btn-outline-light btn-lg px-4">My account</button>
+                </a>
             </div>
         </div>
     </div>
