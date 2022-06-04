2e75fef22ef67856cc465d7d77459b4dada3e844
frank jones <frankjones.web@gmail.com>
2022-06-04 10:44:43 +0100

    Added FontAwesome.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
             }
         </style>
     </head>
-    <body class="antialiased">
+    <body class="antialiased">  
 
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
