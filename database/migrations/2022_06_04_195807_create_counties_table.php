a0252f383e3b44cf358aa1d1b72b576b8cd3f9ee
frank jones <frankjones.web@gmail.com>
2022-06-05 06:31:56 +0100

    Removed seeders for Region, County & Country. We will handle everything in the Constituency seeder.

---
+++ database/migrations/2022_06_04_195807_create_counties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counties', function (Blueprint $table) {
+            $table->id();
+            $table->string('hex', 11);
+            $table->foreignId('country_id')->contrained();
+            $table->foreignId('region_id')->contrained();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('image');
+            $table->timestamps();
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counties');
+    }
+};
